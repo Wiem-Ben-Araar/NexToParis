2fa7b7a1e1fc7ee9c0e45de2bcf4af8caa2b9222
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2024-10-17 23:24:18 +0100

    V4 Deployment

--- src/app/update/page.tsx
+++ src/app/update/page.tsx
@@ -1,15 +1,5 @@
-import { authOptions } from "../../../lib/authOptions";
 import UpdateProfileClient from "./UpdateProfileClient";
-import { getServerSession } from "next-auth/next";
-import { NextResponse } from "next/server";
 
-export default async function UpdateProfilePage() {
-  // Fetch session data on the server
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.redirect("/login");
-  }
-
-  return <UpdateProfileClient session={session} />;
+export default function UpdateProfilePage() {
+  return <UpdateProfileClient />;
 }
